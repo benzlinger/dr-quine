3c38b83f7fed7a899ba22290e13c1733afa546d5
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2024-09-12 15:30:39 +0200

    finished program 2 (C part)

---
+++ C/Grace/Grace.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#define GRACE()int main(){freopen("Grace_kid.c", "w", stdout);char*s="#include <stdio.h>%c#define GRACE()int main(){freopen(%cGrace_kid.c%c, %cw%c, stdout);char*s=%c%s%c;printf(s,10,34,34,34,34,34,s,34,10,10,9,10,10);}%c/*%c%ccomment%c*/%cGRACE()";printf(s,10,34,34,34,34,34,s,34,10,10,9,10,10);}
+/*
+	comment
+*/
+GRACE()
