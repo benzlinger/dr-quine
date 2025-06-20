566c44b97f00b95b2e408e35fdd7adb6d641ab95
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2025-06-20 12:46:42 +0200

    changed to whole program inside a macro

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,40 +1,38 @@
 ; comment
+%macro START 0
 global _main
 extern _fprintf, _fopen, _fclose
-
-%define QUINE "; comment%1$cglobal _main%1$cextern _fprintf, _fopen, _fclose%1$c%1$c%%define QUINE %2$c%3$s%2$c%1$c%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define MODE %2$cw%2$c%1$csection .data%1$cquine db QUINE, 0%1$cfilename db FILENAME, 0%1$cmode db MODE, 0%1$c%1$csection .text%1$c%%macro openfile 0%1$clea rdi, [rel filename]%1$clea rsi, [rel mode]%1$ccall _fopen%1$cmov r12, rax%1$c%%endmacro%1$c%1$c%%macro fprint 1%1$cmov rdi, r12%1$clea rsi, [rel %%1]%1$cmov rdx, 10%1$cmov rcx, 34%1$clea r8, [rel %%1]%1$ccall _fprintf%1$c%%endmacro%1$c%1$c%%macro grace 0%1$center 0, 0%1$copenfile%1$cfprint quine%1$cmov rdi, r12%1$ccall _fclose%1$cleave%1$cret%1$c%%endmacro%1$c%1$c_main: grace%1$c"
-%define FILENAME "Grace_kid.s"
-%define MODE "w"
 section .data
-quine db QUINE, 0
-filename db FILENAME, 0
-mode db MODE, 0
+file db "Grace_kid.s", 0
+mode db "w", 0
+quine db "; comment%1$c%%macro START 0%1$cglobal _main%1$cextern _fprintf, _fopen, _fclose%1$csection .data%1$cfile db %2$cGrace_kid.s%2$c, 0%1$cmode db %2$cw%2$c, 0%1$cquine db %2$c%3$s%2$c, 0%1$c%1$c%%define NL 10%1$c%%define DQ 34%1$c%%define QUINE quine%1$c%1$csection .text%1$c_main:%1$center 0, 0%1$c%1$clea rdi, [rel file]%1$clea rsi, [rel mode]%1$ccall _fopen%1$cmov r12, rax%1$c%1$c%1$cmov rdi, r12%1$clea rsi, [rel QUINE]%1$cmov rdx, NL%1$cmov rcx, DQ%1$clea r8, [rel QUINE]%1$ccall _fprintf%1$c%1$cmov rdi, r12%1$ccall _fclose%1$c%1$cleave%1$cret%1$c%%endmacro%1$c%1$cSTART", 0
+
+%define NL 10
+%define DQ 34
+%define QUINE quine
 
 section .text
-%macro openfile 0
-lea rdi, [rel filename]
+_main:
+enter 0, 0
+
+lea rdi, [rel file]
 lea rsi, [rel mode]
 call _fopen
 mov r12, rax
-%endmacro
 
-%macro fprint 1
+
 mov rdi, r12
-lea rsi, [rel %1]
-mov rdx, 10
-mov rcx, 34
-lea r8, [rel %1]
+lea rsi, [rel QUINE]
+mov rdx, NL
+mov rcx, DQ
+lea r8, [rel QUINE]
 call _fprintf
-%endmacro
 
-%macro grace 0
-enter 0, 0
-openfile
-fprint quine
 mov rdi, r12
 call _fclose
+
 leave
 ret
 %endmacro
 
-_main: grace
+START
