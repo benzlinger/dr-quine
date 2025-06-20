b26975ac03590f95eb5d36bd7833d344006ec095
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2025-06-20 12:47:26 +0200

    changed to exactly 3 defines

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#define GRACE()int main(){freopen("Grace_kid.c", "w", stdout);char*s="#include <stdio.h>%c#define GRACE()int main(){freopen(%cGrace_kid.c%c, %cw%c, stdout);char*s=%c%s%c;printf(s,10,34,34,34,34,34,s,34,10,10,9,10,10);}%c/*%c%ccomment%c*/%cGRACE()";printf(s,10,34,34,34,34,34,s,34,10,10,9,10,10);}
+#define NL 10
+#define TB 9
+#define GRACE() int main(){freopen("Grace_kid.c", "w", stdout);char *s = "#include <stdio.h>%1$c#define NL 10%1$c#define TB 9%1$c#define GRACE() int main(){freopen(%3$cGrace_kid.c%3$c, %3$cw%3$c, stdout);char *s = %3$c%4$s%3$c;printf(s, NL, TB, 34, s);}%1$c/*%1$c%2$ccomment%1$c*/%1$cGRACE()";printf(s, NL, TB, 34, s);}
 /*
 	comment
 */
